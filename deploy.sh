218c4de3313a9ea9a8df74211f8e0627983f4ff4
velesov7493 <velesov7493@gmail.com>
2022-05-11 21:46:10 +0300

    6. Docker и Github [#504782]

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+IMAGE="$DOCKER_USERNAME/job4j_url_shortcut"
+mvn package -Dmaven.test.skip=true ;
+docker build -t ${IMAGE} . ;
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin ;
+docker push ${IMAGE} ;
+exit 0
